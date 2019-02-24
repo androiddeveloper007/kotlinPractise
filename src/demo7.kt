f3e659501c7d48cd06143c834a8c94bbd7cbba54
xiaoxiong <zhu852514500@163.com>
2019-02-24 12:26:04 +0800

    从基本类型到委托模式

---
+++ src/demo7.kt
@@ -0,0 +1,15 @@
+//∑∫–Õ
+fun main(){
+	
+}
+
+class Box<T>(t:T){
+	var value = t
+}
+
+private fun test1(){
+	val box:Box<Int> = Box<Int>(1)
+	//ªÚ’ﬂ
+	val box1 = Box(1)
+	val boxString = Box<String>("aaa")
+}
---
+++ src/demo8.kt
@@ -0,0 +1,8 @@
+//√∂æŸ¿‡
+enum class Color(val rgb:Int){
+	RED(0xff0000),
+	BLACK(0x000000),
+	BLUE(0xff0000),
+	GREEN(0xff0000),
+	WHITE(0xffffff)
+}
